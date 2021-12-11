4f7b1e15d4fbbd21466e453b142c4432a11b5884
Vitaliy <networkvetalius@gmail.com>
2021-12-11 13:27:36 +0200

    02-lightbox done

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,30 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const refs = {
+  gallery: document.querySelector('.gallery'),
+}
+
+refs.gallery.addEventListener('click', onImageClick)
+
+function onImageClick(event) {
+  event.preventDefault();
+  const isClickOnGalleryImage = event.target.classList.contains('gallery__image');
+  if (!isClickOnGalleryImage) {
+    return;
+  }
+}
+
+const galleryMarkup = createGalleryMarkup(galleryItems);
+
+function createGalleryMarkup(galleryItems) {
+  return galleryItems.map(item => {
+  return  `<a class="gallery__item" href="${item.original}">
+  <img class="gallery__image" src="${item.preview}" alt="${item.description}"  />
+</a>`
+}).join('');
+}
+
+refs.gallery.innerHTML = galleryMarkup;
+
+new SimpleLightbox('.gallery a', {captionsData: 'alt', captionDelay: 250 });
